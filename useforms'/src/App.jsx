646f024e62d4437e602fabe950e57e78360d4d74
Nikhil Kumar <nkkr9538@gmail.com>
2024-04-08 15:19:37 +0530

    useforms

---
+++ useforms'/src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import Useform from './Useform';
+import "./App.css"
+function App() {
+  return (
+    <div className="App">
+      <Useform />
+    </div>
+  );
+}
+
+export default App;
